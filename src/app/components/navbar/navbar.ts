a31d4bc84436284f71c970fede2196afc365f140
mkarampatsis <marka@central.ntua.gr>
2026-03-03 10:03:44 +0200

    changes to navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgbCollapseModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [
+    RouterLink, 
+    RouterLinkActive,
+    NgbCollapseModule,
+    NgbDropdownModule
+  ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
-
+  isCollapsed = true;
 }
